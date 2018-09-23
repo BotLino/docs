61efd045f5e0745826144ae88bc079475b82e78e
Guilherme Marques Rosa <marquesguilherme3@gmail.com>
2018-09-23 15:12:28 -0300

    Add doc files and create doc names list

--- src/components/DocIndexPage.js
+++ src/components/DocIndexPage.js
@@ -1,13 +1,45 @@
-import React, { Component } from 'react'
-import Navbar from './navbar/Navbar'
-import './DocIndexPage.css'
+import React, { Component } from "react";
+import Navbar from "./navbar/Navbar";
+import "./DocIndexPage.css";
 
 export default class DocIndexPage extends Component {
   render() {
+    const docs = [
+      { title: "Abertura", contents: ["TAP", "EAP", "Visão Geral do Produto"] },
+      {
+        title: "Projeto",
+        contents: [
+          "Plano Metodológico",
+          "Documento de Visão",
+          "Documento de Arquitetura"
+        ]
+      },
+      { title: "Roadmap", contents: ["Roadmap Geral", "Roadmap dos Papéis"] }
+    ];
+    const list = docs.map((val, i) => {
+      return (
+      <li key={i} className="doc-title">
+          {val.title}
+        <ul className="doc-index">
+          {val.contents.map((content, j) => {
+            return(
+              <li key={j} className='doc-index-item'>{content}</li>
+            )
+          })}
+        </ul>
+      </li>
+      )
+    });
     return (
-      <div className='doc-list'>
+      <div className="doc-list">
         <Navbar active={true} />
+        <div className="row">
+          <div className="col s3 doc-navigation">
+            <ul className="doc-index">{list}</ul>
+          </div>
+          <div className="col s9 doc-content">BBBBBBB</div>
+        </div>
       </div>
-    )
+    );
   }
 }
