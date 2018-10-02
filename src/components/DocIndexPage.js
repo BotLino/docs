7a84f80d66a2a922856fe5e88c15600552559b70
Icaro Oliveira <icarooliv2@gmail.com>
2018-10-02 17:15:01 -0300

    removes warnings

--- src/components/DocIndexPage.js
+++ src/components/DocIndexPage.js
@@ -1,24 +1,22 @@
 import React, { Component } from "react";
-import Navbar from "./navbar/Navbar";
-import DocList from './docs-list/docs-list'
-import Markdown from './markdown/Markdown'
 import "./DocIndexPage.css";
 import DocVisao from "./doc-visao/doc-visao";
 
 export default class DocIndexPage extends Component {
   render() {
-    return <div>
-      <DocVisao />
-    </div>;
+    return (
+      <div>
+        <DocVisao />
+      </div>
+    );
     // <div className="doc-list">
-      //   <Navbar active={true} />
-      //   <div className="row">
-      //     <DocList />
-      //     <div className="col s9 push-s2 doc-content container">
-      //       <h3 className='center aligned'>Documentos</h3>
-      //     </div>
-      //   </div>
-      // </div>
-      
+    //   <Navbar active={true} />
+    //   <div className="row">
+    //     <DocList />
+    //     <div className="col s9 push-s2 doc-content container">
+    //       <h3 className='center aligned'>Documentos</h3>
+    //     </div>
+    //   </div>
+    // </div>
   }
 }
