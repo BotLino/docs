854b935f14e4a8f58166bc06f9a70e02413152e8
Icaro Oliveira <icarooliv2@gmail.com>
2018-10-02 22:59:01 -0300

    updates documento-de-arquitetura.md and fixes css bugs

--- src/components/doc-roadmap-papeis/doc-roadmap-papeis.js
+++ src/components/doc-roadmap-papeis/doc-roadmap-papeis.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import Doc from "../doc-component/doc-component";
+import styles from "../doc-roadmap-papeis/styles.css";
 
 export default class DocRoadmapPapeis extends Component {
   render() {
-    return <Doc source="roadmap-papeis" title="Roadmap de Papeis" />;
+    return (
+      <Doc
+        className={styles.container}
+        source="roadmap-papeis"
+        title="Roadmap de Papeis"
+      />
+    );
   }
 }
