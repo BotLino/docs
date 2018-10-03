4b30552d8ef5b3bc71cd9094f7df097dcc80effe
Guilherme Marques Rosa <marquesguilherme3@gmail.com>
2018-10-03 09:29:14 -0300

    Removes extra paragraph

--- src/components/showcase/showcase.js
+++ src/components/showcase/showcase.js
@@ -20,11 +20,6 @@ export default class Showcase extends Component {
                   envio com o maior prazer ;{")"}
               </div>
           </div>
-          <div className="showcase-text">
-            Quando estiver com fome e naquela dúvida se come no RU, basta
-            falar comigo! Eu pesquiso qual será o cardápio de hoje e te envio
-            com o maior prazer ;{")"}
-          </div>
         </ScrollAnimation>
         <ScrollAnimation animateIn='slideInRight' animateOnce={true} offset={10}>
             <img className="col s4 push-s2 showcase-img" src={show} />
