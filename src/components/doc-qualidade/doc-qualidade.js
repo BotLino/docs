44a76f0f4fd27d07ec90b96d70c367a296f73aa7
Bruna Pinos <bruna.pinos@gmail.com>
2018-11-29 11:08:16 -0200

    Create doc-qualidade.js

---
+++ src/components/doc-qualidade/doc-qualidade.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import Doc from '../doc-component/doc-component'
+
+export default class DocQualidade extends Component {
+  render() {
+    return <Doc source='qualidade' title='Documento de Qualidade'/>
+  }
+}
